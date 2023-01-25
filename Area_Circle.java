0d7cfe3378b6097208d43a80332100f4e869e77c
Anas Jawed <103234658+anasjawed283@users.noreply.github.com>
2023-01-25 19:58:08 +0530

    PAT
    Read the radius as double value and calculate the area of Circle using Java.
    Sample Input
    10
    Sample Output
    314

---
+++ Area_Circle.java
@@ -0,0 +1,9 @@
+//Read description for question//
+import java.util.Scanner;
+class Main{
+  public static void main(String args[]){
+    Scanner obj = new Scanner(System.in);
+    double radius = obj.nextDouble();
+    System.out.println(3.14*radius*radius);
+  }
+}
