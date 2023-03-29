32ad78649689789a9e201e9726082fb13f7c8f31
Anas Jawed <103234658+anasjawed283@users.noreply.github.com>
2023-03-29 13:29:48 +0530

    MAT
    Read two numbers from user. Perform division operation and print the output. In the hidden testcase, the dividend is a valid number and the divisor is zero. 
    In spite of divisor being zero, your code should not raise exception. How can you do that?

---
+++ Make_division_by_0.java
@@ -0,0 +1,14 @@
+//Read description for question
+import java.util.*;
+class Main{
+    public static void main(String s[]){
+        double a,b;
+        Scanner ss=new Scanner(System.in);
+        a=ss.nextDouble();
+        b=ss.nextDouble();
+    
+            double c=a/b;
+            System.out.println(c);
+        
+    }
+}
